beca7738ca891bbc51e1a58564c1b0ff6ab19d02
DevilScript <52322852+DevilScript@users.noreply.github.com>
2025-05-05 18:50:57 +0700

    d

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -538,7 +538,7 @@ const Admin = () => {
                       id="kaguneV2" 
                       checked={rcFormData.isKaguneV2}
                       onCheckedChange={(checked) => 
-                        setRc 阳FormData({...rcFormData, isKaguneV2: checked as boolean})
+                        setRc FormData({...rcFormData, isKaguneV2: checked as boolean})
                       }
                       className="border-pink-300/30 data-[state=checked]:bg-pink-300 data-[state=checked]:border-pink-300"
                     />
